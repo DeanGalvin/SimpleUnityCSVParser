6d8b337af8d5a0287d4f6b867c1506c9df9d49dc
DeanGalvin <deangalvin10@gmail.com>
2016-10-23 00:40:11 +0100

    Added CSVReader.cs file

---
+++ CSVReader.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.IO;
+
+public class CSVReader : MonoBehaviour {
+
+	public TextAsset csvFile;
+	public GameObject objectToSpawn;
+	private char lineSeperator = '\n';
+	private char fieldSeperator = ',';
+
+	private string[] records;
+	private string[] fields;
+
+	void Start()
+	{
+		spawnObjectsFromCSV ();
+	}
+
+	//Spawn the intended objects from the parsed locations.
+	private void spawnObjectsFromCSV()
+	{
+		records = csvFile.text.Split (lineSeperator);
+		for (int i = 0; i < records.Length; i++) {
+			fields = records [i].Split (fieldSeperator);
+			float x = float.Parse(fields [0]);
+			float y = float.Parse(fields [1]);
+			float z = float.Parse(fields [2]);
+			Instantiate (objectToSpawn, new Vector3 (x, y, z), Quaternion.identity);
+		}
+	}
+
+}
